a36d77ccb12d09d856201dc165747d481aa99d8d
Joon <joon731@korea.ac.kr>
2023-11-09 13:45:36 +0900

    Upload src files

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.static('views/images'))
+app.use(express.urlencoded({ extended: false }))
+
+const webPath = path.join(__dirname, '../views')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+
+app.set('view engine', 'hbs')
+app.set('views', webPath)
+app.use(express.static(publicPath))
+
+app.get('/', (req, res) => {
+    res.render('login')
+})
+
+app.post('/login', async (req, res) => {
+
+    try {
+        const check = await LogInCollection.findOne({ name: req.body.name })
+
+        if (check.password === req.body.password) {
+            res.render('home')
+        }
+
+        else {
+            res.send("incorrect password")
+        }
+
+
+    } 
+    
+    catch (e) {
+        res.send("wrong details")
+    }
+
+
+})
+
+app.listen(port, () => {
+    console.log('port connected');
+})
---
+++ src/mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+
+mongoose.connect("mongodb+srv://root:password1234@cluster0.tqxkiwp.mongodb.net/?retryWrites=true&w=majority")
+.then(()=>{
+    console.log('mongoose connected');
+})
+.catch((e)=>{
+    console.log('failed');
+})
+
+const logInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
+
+module.exports=LogInCollection
